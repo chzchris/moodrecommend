abbde1c77f603ae5531a3b1a25e5c7b1bb05c72b
WaiYu Hsieh <waiyuhsieh@gmail.com>
2015-07-30 22:59:34 -0700

    Front end update

--- controllers/static/js/process.js
+++ controllers/static/js/process.js
@@ -39,21 +39,42 @@ function getRecommend() {
 
 	var req = new XMLHttpRequest();
 
-	req.open("GET", query, true);
-	req.send();
+	//req.open("GET", query, true);
+	//req.send();
 
 	//alert(req.responseText)
-	
+
+	var resultJSON = {"artist":"Jason Mraz", "song":"I'm Yours", "cover_url":"http://akamai-b.cdn.cddbp.net/cds/2.0/cover/AF0A/365F/FDE4/F518_medium_front.jpg"};
+	var ele = document.getElementById("playlist");
+	var result = document.createElement('li');
+	var coverArt = document.createElement('li');
+	result.className = "track";
+	result.appendChild(document.createTextNode(resultJSON.song + " - " + resultJSON.artist));
+	ele.appendChild(result);
+	/*
 	// Create the callback
 	req.onreadystatechange = function() {
 		if (req.readyState==4 && req.status == 200) {
 		// Request successful, read the response
+		
 		var ele = document.getElementById("recommend");
 		ele.innerHTML = "<pre>"+req.responseText+"</pre>";
 		ele.style.display = "block";
+
+		//var textresponse = req.responseText;
+		//var resultJSON = JSON.parse(textresponse);
+		var resultJSON = JSON.parse(req.responseTex);
+		alert(Object.keys(resultJSON));
+		
+		var ele = document.getElementById("playlist");
+		var result = document.createElement('li');
+		result.className = "track";
+		result.appendChild(document.createTextNode(resultJSON.song + " - " + resultJSON.artist));
+		ele.appendChild(result);
 		}
 	}
+	*/
 
-	alert(query)
+	//alert(query)
 }
 
